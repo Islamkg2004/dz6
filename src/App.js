a05f5c56ab3a8e102f94aa6caf3f49e716b880a5
Islamkg2004 <islam.kg.2004@gmail.com>
2023-03-03 23:13:34 +0600

    dzz6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [name] = useState(['Арген', 'Максат', 'Жанылай'  ]);
+
+    return(
+        <ul>
+            {name.map(name =>(
+                <li key={name}>{name}</li>
+            ))}
+        </ul>
+    )
 }
-
-export default App;
